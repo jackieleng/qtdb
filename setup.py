f88eda15bd39e8ad0f202d0753de2a030c079a81
Jackie <Jackie.Leng@nelen-schuurmans.nl>
2017-01-24 17:18:45 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='qtdb',
+      version='0.1',
+      description='pdb for pyqt4',
+      url='https://github.com/jackieleng/qtdb',
+      author='Jackie Leng',
+      author_email='jackie.leng@nelen-schuurmans.nl',
+      license='TODO',
+      packages=['qtdb'],
+      zip_safe=False)
